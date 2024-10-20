06eca67a7aac1a656533af95ed434c1a3471fcc5
TAIF <taif-alsadoon@outlook.com>
2024-10-20 16:46:48 +0300

    Initial Commit

---
+++ Plant/PlantApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantApp.swift
+//  Plant
+//
+//  Created by Taif Alsadoon on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct PlantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlantTests/PlantTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlantTests.swift
+//  PlantTests
+//
+//  Created by Taif Alsadoon on 17/04/1446 AH.
+//
+
+import Testing
+@testable import Plant
+
+struct PlantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
